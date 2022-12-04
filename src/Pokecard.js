b5912c7cb298bce9d4eafc745b9d54815b12fae7
Jacob Barrow <116927191+jacobbarrow98@users.noreply.github.com>
2022-12-04 11:16:15 -0600

    new file

---
+++ src/Pokecard.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+
+
+
+
+class Pokecard extends Component{
+    static defaultProps = [
+  {id: 392, name: 'Infernape', type: 'fire/fighting', base_experience: 209},
+  {id: 260, name: 'Swampert', type: 'water/ground', base_experience: 210},
+  {id: 168, name: 'Ariados', type: 'bug/poison', base_experience: 134},
+  {id: 282, name: 'Gardevoir', type: 'psychic', base_experience: 208},
+  {id: 405, name: 'Luxray', type: 'electric', base_experience: 194},
+  {id: 350, name: 'Milotic', type: 'water', base_experience: 213},
+  {id: 491, name: 'Darkrai', type: 'dark', base_experience: 210},
+  {id: 133, name: 'Eevee', type: 'normal', base_experience: 65},
+  {id: 493, name: 'Arceus', type: 'normal', base_experience: 255},  
+  {id: 386, name: 'Deoxys', type: 'psychic', base_experience: 215},  
+  {id: 244, name: 'Entei', type: 'fire', base_experience: 217},  
+  {id: 477, name: 'Dusknoir', type: 'ghost', base_experience: 210}
+]
+    return ();
+}
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Pokecard from "./Pokecard";
+
+class Pokedex extends React.Component {
+    return (
+        <Pokecard/>
+    );
+}
+
+export default Pokedex;
