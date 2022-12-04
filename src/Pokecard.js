d61f6f3ef0f6191382772bb413a5faa8f2399bab
Jacob Barrow <116927191+jacobbarrow98@users.noreply.github.com>
2022-12-04 12:43:57 -0600

    xc

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,25 +1,25 @@
-import React, {Component} from "react";
+import React from 'react';
+import './Pokecard.css';
 
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
-
-
-
-class Pokecard extends Component{
-    static defaultProps = [
-  {id: 392, name: 'Infernape', type: 'fire/fighting', base_experience: 209},
-  {id: 260, name: 'Swampert', type: 'water/ground', base_experience: 210},
-  {id: 168, name: 'Ariados', type: 'bug/poison', base_experience: 134},
-  {id: 282, name: 'Gardevoir', type: 'psychic', base_experience: 208},
-  {id: 405, name: 'Luxray', type: 'electric', base_experience: 194},
-  {id: 350, name: 'Milotic', type: 'water', base_experience: 213},
-  {id: 491, name: 'Darkrai', type: 'dark', base_experience: 210},
-  {id: 133, name: 'Eevee', type: 'normal', base_experience: 65},
-  {id: 493, name: 'Arceus', type: 'normal', base_experience: 255},  
-  {id: 386, name: 'Deoxys', type: 'psychic', base_experience: 215},  
-  {id: 244, name: 'Entei', type: 'fire', base_experience: 217},  
-  {id: 477, name: 'Dusknoir', type: 'ghost', base_experience: 210}
-]
-    return ();
+class Pokecard extends React.Component {
+  render() {
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    return (
+      <div className="Pokecard">
+        <h3 className="Pokecard-title">{this.props.name}</h3>
+        <img
+          className="Pokecard-img"
+          src={imgSrc}
+          alt={this.props.name}
+        />
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
+      </div>
+    );
+  }
 }
 
 export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,42 @@
-import React from "react";
-import Pokecard from "./Pokecard";
+import React from 'react';
+import './Pokedex.css';
+import Pokecard from './Pokecard';
 
 class Pokedex extends React.Component {
+  static defaultProps = {
+    pokemon: [
+      {
+        id: 392,
+        name: 'Infernape',
+        type: 'fire/fighting',
+        base_experience: 209,
+      },
+      { id: 260, name: 'Swampert', type: 'Water/Ground', base_experience: 210 },
+      { id: 282, name: 'Gardevoir', type: 'Psychic', base_experience: 208 },
+      { id: 350, name: 'Milotic', type: 'Water', base_experience: 213 },
+      { id: 491, name: 'Darkrai', type: 'Dark', base_experience: 210 },
+      { id: 493, name: 'Arceus', type: 'Normal', base_experience: 255 },
+      { id: 386, name: 'Deoxys', type: 'Psychic', base_experience: 215 },
+      { id: 477, name: 'Dusknoir', type: 'Ghost', base_experience: 210 },
+    ],
+  };
+  render() {
     return (
-        <Pokecard/>
+      <div className="Pokedex">
+        <h1>Pokedex</h1>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
     );
+  }
 }
 
 export default Pokedex;
