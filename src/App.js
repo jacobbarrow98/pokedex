ad392e6e46f6abc8cb0d3e2cd97f8b2e7b8af08d
Jacob Barrow <116927191+jacobbarrow98@users.noreply.github.com>
2023-01-17 20:46:31 -0600

    Add state to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import './index.css';
+
 function App() {
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    'https://pokeapi.co/api/v2/pokemon?limit=20'
+  );
+
+  const getAllPokemon = async () => {
+    const res = await fetch(loadMore);
+    const data = res.json();
+  };
+
   return (
-    <div className="App">
+    <div className="app-container">
       <h1>Pokedex</h1>
+      <div className="pokemon-container">
+        <div className="all-container"></div>
+        <button className="load-more">Load more</button>
+      </div>
     </div>
   );
 }
